c6d5ea4af77e3861a1ff9c55809e015d58cf695b
Avinash Prabhakaran <zi.avi7@gmail.com>
2021-09-13 00:37:49 -0400

    Split title

--- src/data/books/ezhuthachante-kala.js
+++ src/data/books/ezhuthachante-kala.js
@@ -6,7 +6,8 @@ import cover from "assets/books-cover/ezhuthachante-kala.jpg";
 const Contents = () => {
 	return (
 		<>
-			<h1>EZHUTHACHANTE KALA - CHILA VYASABHARATHA PADANANGALUM</h1>
+			<h1>EZHUTHACHANTE KALA :</h1>
+			<h1 style={{marginTop: 0}}>CHILA VYASABHARATHA PADANANGALUM</h1>
 			<p>P K Balakrishnan has studied the Indian epics in great depth.  He had Malayalam and English translations of the Mahabharata, some summary translations as also a few of the regional variants of both epics in his personal library.  Ezhuthachan is considered as the father of Malayalam literature. Though serious studies have been conducted, no one has dealt with how a &apos;translator&apos; of Mahabharath becomes the foremost poet and father of Malayalam literature. Ezhuthachan&apos;s Bharatham is totally different from the Mahabharth in its perception of life and the character structures. The story and event sequence with their relative importance in the original is totally rejected by Ezhuthachan. The first nine chapters of the book deal with this vital distinction. This is followed by a chapter on Adhyatma Ramayanam. The next four chapters deal with the peculiarities of the artistic genius of Ezhuthachan. To complement the early references to the Vyasabharath, the last chapter deals with the evolution of the Vyasabharath.</p>
 			<p>The preface to the book is written by the author&apos;s poet friend Mr. Neelamperur Madhudoodanan Nair.</p>
 		</>
