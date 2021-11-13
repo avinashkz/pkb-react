85e7f1e3a33efca2d971ccddaabff1e62dda8e9d
Avinash Prabhakaran <zi.avi7@gmail.com>
2021-11-13 14:19:02 -0500

    Remove modal commented out code

--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -12,51 +12,18 @@ const Container = styled.div`
 	grid-column-gap: min(5%, 40px);
 `;
 
-// function MyVerticallyCenteredModal(props) {
-// 	return (
-// 	  <Modal
-// 		{...props}
-// 		size="lg"
-// 		aria-labelledby="contained-modal-title-vcenter"
-// 		centered
-// 	  >
-// 		<Modal.Header closeButton>
-// 		  <Modal.Title id="contained-modal-title-vcenter">
-// 			Modal heading
-// 		  </Modal.Title>
-// 		</Modal.Header>
-// 		<Modal.Body>
-// 		  <h4>Centered Modal</h4>
-// 		  <p>
-// 			Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-// 			dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-// 			consectetur ac, vestibulum at eros.
-// 		  </p>
-// 		</Modal.Body>
-// 		<Modal.Footer>
-// 		  <Button onClick={props.onHide}>Close</Button>
-// 		</Modal.Footer>
-// 	  </Modal>
-// 	);
-//   }]
-
-
 function importAll(r) {
 	return r.keys().map(r);
-  }
+}
 
 function Gallery() {
 	const thumbnails = importAll(require.context('assets/gallery/3x4', false, /\.(png|jpe?g|svg)$/));
-	// const fullsize = importAll(require.context('assets/gallery/fullsize', false, /\.(png|jpe?g|svg)$/));
 	return (
 		<Container>
 			{thumbnails.map((image, index) => (
-				<>
-					{/* <Card> */}
-					<Card.Img key={index} src={image.default}/>
-						{/* <Card.Img key={index} src={fullsize[index].default} /> */}
-					{/* </Card> */}
-					</>
+					<Card>
+						<Card.Img key={index} src={image.default} />
+					</Card>
 			))}
 		</Container>);
 }
