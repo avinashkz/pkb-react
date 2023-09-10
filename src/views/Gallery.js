babc801e907f4fb861431a079c4d95f50bfbf1ce
Avinash Prabhakaran <zi.avi7@gmail.com>
2023-09-09 23:56:57 -0400

    Upgrade to React 18

--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -1,35 +1,38 @@
 import React from "react";
 import styled from "styled-components/macro";
-import Card from 'react-bootstrap/Card'
+import Card from "react-bootstrap/Card";
 
 const Container = styled.div`
-	margin: 50px;
-	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
-	justify-self: stretch;
-	justify-items: center;
-	grid-row-gap: 40px;
-	grid-column-gap: min(5%, 40px);
+  margin: 50px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+  justify-self: stretch;
+  justify-items: center;
+  grid-row-gap: 40px;
+  grid-column-gap: min(5%, 40px);
 `;
 
 const Image = styled(Card.Img)`
-	padding: 3px;
+  padding: 3px;
 `;
 
 function importAll(r) {
-	return r.keys().map(r);
+  return r.keys().map(r);
 }
 
 function Gallery() {
-	const thumbnails = importAll(require.context('assets/gallery/3x4', false, /\.(png|jpe?g|svg)$/));
-	return (
-		<Container>
-			{thumbnails.map((image, index) => (
-				<Card>
-					<Image key={index} src={image.default} />
-				</Card>
-			))}
-		</Container>);
+  const thumbnails = importAll(
+    require.context("assets/gallery/3x4", false, /\.(png|jpe?g|svg)$/)
+  );
+  return (
+    <Container>
+      {thumbnails.map((image, index) => (
+        <Card key={index}>
+          <Image key={index} src={image} />
+        </Card>
+      ))}
+    </Container>
+  );
 }
 
 export default Gallery;
